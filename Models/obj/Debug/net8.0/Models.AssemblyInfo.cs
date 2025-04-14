bd3f0ffa5e930b99b1157a0bc4671326453ecc90
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-14 10:10:51 +0200

    build changes

--- Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Debug/net8.0/Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457e1a7d4ebf0e2325784f881b86b79685b08621")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae8ed869e3ffe0766c5a8b6e983a77c7dd77e1e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
