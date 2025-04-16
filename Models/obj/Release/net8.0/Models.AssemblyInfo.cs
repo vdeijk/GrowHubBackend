d8e28a4b5d8d06e59f2287211577baa73f5d0e02
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-16 10:47:24 +0200

    improved dockerfile

--- Models/obj/Release/net8.0/Models.AssemblyInfo.cs
+++ Models/obj/Release/net8.0/Models.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ef3bb21afa27f67bb37a0f73585621e3adc5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f8424a7327cab1908c3167072c76fab5e0562f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
