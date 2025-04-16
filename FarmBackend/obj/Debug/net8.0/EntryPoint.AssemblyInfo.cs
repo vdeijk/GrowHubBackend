f9dcc62002b8dcb70538a33fdd8b22bcaa2da6f3
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-16 14:04:50 +0200

    changes

--- FarmBackend/obj/Debug/net8.0/EntryPoint.AssemblyInfo.cs
+++ FarmBackend/obj/Debug/net8.0/EntryPoint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EntryPoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a1c7bf058ae241fe4b55a88cf65e60241dfb881")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ce78c334d5c9e946aee103ca42507c3b187df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EntryPoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EntryPoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ef3bb21afa27f67bb37a0f73585621e3adc5a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ce78c334d5c9e946aee103ca42507c3b187df0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
