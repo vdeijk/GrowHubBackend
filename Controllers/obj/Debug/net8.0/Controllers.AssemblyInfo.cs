22e0befc8544a1ca17dec35b902e0f461351697f
Rick van der Eijk <vandereijkwebdevelopment@gmail.com>
2025-04-18 18:05:16 +0200

    dockerfile removed

--- Controllers/obj/Debug/net8.0/Controllers.AssemblyInfo.cs
+++ Controllers/obj/Debug/net8.0/Controllers.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Controllers")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9dcc62002b8dcb70538a33fdd8b22bcaa2da6f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5ab6b4a30e12d78651d004f9b5988333e37f421")]
 [assembly: System.Reflection.AssemblyProductAttribute("Controllers")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Controllers")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
